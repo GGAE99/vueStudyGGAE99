4081f3c5578ea21bd165a30a66fcf9d99cbc6329
GGAE99 <duddn1529@gmail.com>
2024-08-09 08:48:56 +0900

    7-1

--- composition-vue-1/src/App.vue
+++ composition-vue-1/src/App.vue
@@ -2,7 +2,8 @@
 import DataEx1 from './components/DataEx1.vue';
 import DataEx2 from './components/DataEx2.vue';
 import DataEx3 from './components/DataEx3.vue';
+import DataEx4 from './components/DataEx4.vue';
 </script>
 <template>
-  <DataEx1 />
+  <DataEx4 />
 </template>
