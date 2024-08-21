9c765a50c83f4d29dc1b5b587d5bc35230f9869e
GGAE99 <duddn1529@gmail.com>
2024-08-22 07:30:59 +0900

    vue study

--- composition-vue-1/src/App.vue
+++ composition-vue-1/src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
+import ComputedText from './components/ComputedText.vue';
 import DataEx1 from './components/DataEx1.vue';
 import DataEx2 from './components/DataEx2.vue';
 import DataEx3 from './components/DataEx3.vue';
 import DataEx4 from './components/DataEx4.vue';
+import MethodComponent from './components/MethodComponent.vue';
 </script>
 <template>
+  <MethodComponent />
+  <ComputedText />
   <DataEx4 />
 </template>
