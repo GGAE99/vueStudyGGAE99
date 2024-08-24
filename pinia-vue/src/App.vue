101ca53e7b50d7a52448e9de057ead551a49ce1f
GGAE99 <duddn1529@gmail.com>
2024-08-24 23:20:16 +0900

    my vue study

--- pinia-vue/src/App.vue
+++ pinia-vue/src/App.vue
@@ -6,5 +6,7 @@ import { RouterLink, RouterView } from 'vue-router';
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
   </nav>
+  <span>beforeView</span>
   <RouterView />
+  <span>afterView</span>
 </template>
